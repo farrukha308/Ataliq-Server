e12312a1970f05fe3a2f7ec26dd7e434de71c627
farrukha330033 <farrukh.aslam@telligenttech.com>
2025-01-02 04:05:24 +0500

    WIP: fix crash

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -57,14 +57,14 @@ const validateToken = async function (
 
         if (!session) {
           auditLog(`Session not found for token: ${token}`);
-          res.status(401).json({ message: 'Session not found or expired', responseCode: 'SE01'}).send('Session not found or expired');
+          res.status(401).json({ message: 'Session not found or expired', responseCode: 'SE01'}).send();
           next()
         }
 
         // Check if session is expired
         if (session?.expiresAt < new Date()) {
           auditLog(`Session expired for token: ${token}`);
-          res.status(401).json({ message: 'Session expired' , responseCode: 'SE01'}).send('Session not found or expired');;
+          res.status(401).json({ message: 'Session expired' , responseCode: 'SE01'}).send();
           next()
         }
 
